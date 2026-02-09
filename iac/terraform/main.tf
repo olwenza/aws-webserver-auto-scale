6ceeaf2210fd7d0de25234277c48923be412afb1
Ivan Augustino <olwenza@yahoo.com>
2026-02-09 09:01:24 -0600

    Add alb listener

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -121,3 +121,15 @@ resource "aws_lb_target_group" "app_tg" {
     unhealthy_threshold = 2
   }
 }
+
+# Load balancer listeners - listen to traffic from ALB distribute to target group members
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
